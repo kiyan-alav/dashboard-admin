0faab323db7f070867fcc52d73c5209364e3de91
Kiyan Alavi <81843676+kiyan-alav@users.noreply.github.com>
2023-10-29 18:48:35 +0330

    Update supabaseClient.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://lvehxnsnmusbusqmaebn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2ZWh4bnNubXVzYnVzcW1hZWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0NDE1NDEsImV4cCI6MTk5MjAxNzU0MX0.9FIagetOnY5NPukK71-JKO42TtdxR7vlh3W6B0aUojo";
+const supabaseKey = "YOUR_API_KEY";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
